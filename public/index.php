571c04b995b821f38766baf9fdc915ef6719b70a
Mahmut Yüksel Mert <mahmutyukselmert@gmail.com>
2025-08-26 00:38:49 +0300

    Proje temeli kopyaladım

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php require 'init.php'; ?>
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABT Global</title>
+    <?php include 'include/head.html'; ?>
+</head>
+
+<body>
+    <?php include 'include/header.html'; ?>
+    <?php include 'include/hero-section.html'; ?>
+
+    
+
+    <?php include 'include/footer.html'; ?>
+</body>
+</html>
